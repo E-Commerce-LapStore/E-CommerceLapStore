dc676a0be93c33905ac67dd476cb9539652b7ddd
WaelAlfanan <waelbahaa01@hotmail.com>
2025-03-25 19:33:56 +0000

    Updated Settings

---
+++ LapStore/LapStore.DAL/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LapStore.DAL.Entities
+{
+    internal class User
+    {
+        public int Id { get; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int 
+
+    }
+}
