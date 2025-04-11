10795f25f6bbddf2685d035bdaa0b125d07ced2d
WaelAlfanan <waelbahaa01@hotmail.com>
2025-04-11 15:32:54 +0100

    Updated Title

--- LapStore/LapStore.Web/obj/Debug/net9.0/LapStore.Web.AssemblyInfo.cs
+++ LapStore/LapStore.Web/obj/Debug/net9.0/LapStore.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LapStore.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f218f3c3a6e52c1e426487731414f163c02d8d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0423593051f0bb15670154de2b57989a676bb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LapStore.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LapStore.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
