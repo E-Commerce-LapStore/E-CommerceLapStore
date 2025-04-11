5f06293d59012ff737394df9b2480ddf9a71949c
WaelAlfanan <waelbahaa01@hotmail.com>
2025-04-12 00:40:52 +0100

    Completed CRUD Operations On Products, Categories

--- LapStore/LapStore.BLL/obj/Debug/net9.0/LapStore.BLL.AssemblyInfo.cs
+++ LapStore/LapStore.BLL/obj/Debug/net9.0/LapStore.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LapStore.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0423593051f0bb15670154de2b57989a676bb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10795f25f6bbddf2685d035bdaa0b125d07ced2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LapStore.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LapStore.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LapStore/LapStore.Web/Controllers/CategoryController.cs
+++ LapStore/LapStore.Web/Controllers/CategoryController.cs
@@ -14,12 +14,10 @@ namespace LapStore.Web.Controllers
             _unitOfWork = unitOfWork;
         }
 
-
         public IActionResult Add()
         {
             return View();
         }
-
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -52,11 +50,61 @@ namespace LapStore.Web.Controllers
 
             return View(categoryVM);
         }
+
         public async Task<IActionResult> Index()
         {
             var categories = await _unitOfWork.BaseRepository<Category>().GetAllAsync();
             var categoryVMs = categories.Select(CategoryVM.FromCategory).ToList();
             return View(categoryVMs);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _unitOfWork.BaseRepository<Category>().GetByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            var categoryVM = CategoryVM.FromCategory(category);
+            return View(categoryVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, CategoryVM categoryVM)
+        {
+            if (id != categoryVM.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var category = CategoryVM.FromCategoryVM(categoryVM);
+                _unitOfWork.BaseRepository<Category>().Update(category);
+                await _unitOfWork.CompleteAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _unitOfWork.BaseRepository<Category>().GetByIdAsync(id);
+            if (category != null)
+            {
+                _unitOfWork.BaseRepository<Category>().Delete(category);
+                await _unitOfWork.CompleteAsync();
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
