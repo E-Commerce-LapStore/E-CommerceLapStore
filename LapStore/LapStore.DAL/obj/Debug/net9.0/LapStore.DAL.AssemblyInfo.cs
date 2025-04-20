92f0dd38c9c2c2761141adb8882725da363f7a78
WaelAlfanan <189515907+WaelAlfnan@users.noreply.github.com>
2025-04-20 04:00:12 +0100

    Updated the Home page And Edited the layout

--- LapStore/LapStore.DAL/obj/Debug/net9.0/LapStore.DAL.AssemblyInfo.cs
+++ LapStore/LapStore.DAL/obj/Debug/net9.0/LapStore.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LapStore.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed8b96028d885895a252d5c9a2620f18b0b23ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aeb5d3566a0f3d41bbea77ccf8324a6bb5f85ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("LapStore.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LapStore.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LapStore/LapStore.Web/Controllers/HomeController.cs
+++ LapStore/LapStore.Web/Controllers/HomeController.cs
@@ -1,21 +1,50 @@
 using System.Diagnostics;
 using LapStore.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using LapStore.BLL.Interfaces;
+using System.Linq;
 
 namespace LapStore.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            ICategoryService categoryService,
+            IProductService productService)
         {
             _logger = logger;
+            _categoryService = categoryService;
+            _productService = productService;
         }
         
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var homeVM = new HomeVM();
+            
+            // Get all categories
+            var categories = await _categoryService.GetAllCategoriesAsync();
+            homeVM.Categories = categories.Select(CategoryVM.FromCategory);
+
+            // Get all products and take the newest 4
+            var products = await _productService.GetAllProductsAsync();
+            homeVM.NewProducts = products
+                .OrderByDescending(p => p.Id) // Assuming Id is auto-incremented
+                .Take(4)
+                .Select(ProductVM.FromProduct);
+
+            // For now, we'll just take 4 random products as top selling
+            // In a real application, this would be based on actual sales data
+            homeVM.TopSellingProducts = products
+                .OrderBy(x => Guid.NewGuid())
+                .Take(4)
+                .Select(ProductVM.FromProduct);
+
+            return View(homeVM);
         }
         
         public IActionResult Category()
